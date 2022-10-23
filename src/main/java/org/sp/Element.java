a6ac016287e7b914e005bb7d91be66462d362a23
AndreeaBrandusa <andreea.brandusa01@e-uvt.ro>
2022-10-23 22:59:57 +0300

    Add Element Interface

---
+++ src/main/java/org/sp/Element.java
@@ -0,0 +1,8 @@
+package org.sp;
+
+public interface Element {
+    public void print();
+    public void add(Element element);
+    public void remove(Element element);
+    public Element get(int elIndex);
+}
