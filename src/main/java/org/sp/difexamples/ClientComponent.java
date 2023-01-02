3cfab7b5c8b98b26330abda9b64fba3aa8927683
AndreeaBrandusa <andreea.brandusa01@e-uvt.ro>
2023-01-02 15:24:18 +0200

    Add Dependecy Injection Design Pattern

--- src/main/java/org/sp/difexamples/ClientComponent.java
+++ src/main/java/org/sp/difexamples/ClientComponent.java
@@ -1,9 +1,12 @@
 package org.sp.difexamples;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ClientComponent {
     private final TransientComponent tc;
     private final SingletonComponent sc;
--- src/main/java/org/sp/difexamples/HelloController.java
+++ src/main/java/org/sp/difexamples/HelloController.java
@@ -1,21 +1,23 @@
 package org.sp.difexamples;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class HelloController {
-    ClientComponent client;
-
-    public HelloController(ClientComponent client) {
-        this.client = client;
-    }
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class HelloController implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from ClientComponent = " + client;
+        return "Hello from ClientComponent = " + applicationContext.getBean(ClientComponent.class);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
