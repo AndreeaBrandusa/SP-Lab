485a258b15fd3b7f1c2b45d437d93a92f64634e8
AndreeaBrandusa <andreea.brandusa01@e-uvt.ro>
2023-01-03 22:47:01 +0200

    Add JPA Persistence

--- src/main/java/org/sp/models/Author.java
+++ src/main/java/org/sp/models/Author.java
@@ -1,6 +1,18 @@
 package org.sp.models;
 
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(force = true)
 public class Author {
+    @Id
+    @GeneratedValue
+    private int id;
     private final String name;
     private final String surname;
 
@@ -8,6 +20,14 @@ public class Author {
         String[] name_list = name.split(" ");
         this.name = name_list[0];
         this.surname = name_list[1] + " " + name_list[2];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
     }
 
     // get first name
